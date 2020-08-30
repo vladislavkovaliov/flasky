ed6d27026494415ed73798576006b4069ce1f5e9
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2020-08-30 13:39:02 +0300

    adds flash and session

--- hello.py
+++ hello.py
@@ -1,17 +1,43 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "hard to guess string"
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    form = NameForm()
+
+    if form.validate_on_submit():
+        old_name = session.get("name")
+
+        if old_name is not None and old_name != form.name.data:
+            flash("Look like you have changed your name!")
+
+        session["name"] = form.name.data
+
+        return redirect(url_for("index"))
+
     return render_template(
         "index.html",
+        form=form,
+        name=session.get("name"),
         current_time=datetime.utcnow()
     )
 
