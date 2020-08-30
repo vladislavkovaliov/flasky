185a4d0e9925a69961b8b75923f48b181ab6ad11
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2020-08-30 19:32:24 +0300

    adds sync/async mailing by flask mail

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 import os
+from threading import Thread
 
 from flask import Flask, render_template, session, url_for
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail, Message
 from flask_migrate import Migrate, MigrateCommand
 from flask_moment import Moment
 from datetime import datetime
@@ -21,12 +23,22 @@ app.config["SECRET_KEY"] = "hard to guess string"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["FLASKY_MAIL_SUBJECT_PREFIX"] = "[Flasky]"
+app.config["FLASKY_MAIL_SENDER"] = "Flasky Admin <flasky@example.com>"
+app.config["FLASKY_ADMIN"] = os.environ.get("FLASKY_ADMIN")
+
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 manager = Manager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -69,6 +81,26 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
 
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(
+        app.config["FLASKY_MAIL_SUBJECT_PREFIX"] + subject,
+        sender=app.config["FLASKY_MAIL_SENDER"],
+        recipients=[to]
+    )
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+
+    return thr
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
@@ -80,6 +112,14 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session["known"] = False
+            print(app.config["FLASKY_ADMIN"])
+            if app.config["FLASKY_ADMIN"]:
+                send_email(
+                    app.config["FLASKY_ADMIN"],
+                    "New User",
+                    "mail/new_user",
+                    user=user
+                )
         else:
             session["known"] = True
 
